a18af0c040c174a83bc4574ea8f874d0f594e1a8
Tony Kunz <42658482+cryptotwilight@users.noreply.github.com>
2020-10-11 20:31:21 +0100

    Updated minor updates

--- CryptoFlowInvestmentManager.sol
+++ CryptoFlowInvestmentManager.sol
@@ -1,30 +1,29 @@
+//SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.6.12;
 
-//import 
-
-import "ICFProduct.sol"; 
+import "./ICFProduct.sol"; 
 
 contract CryptoFlowInvestmentManager{         //declare contract
       
-       address payable momsWallet;
-       address cryptoFlowAddress;
-       ICFProduct cryptoFlow;
+    address payable momsWallet;
+    address cryptoFlowAddress;
+    ICFProduct cryptoFlow;
        
-       constructor(address _momsWallet, address _cryptoFlow) public payable {
-               momsWallet = _momsWallet;
-               cryptoFlowAddress = _cryptoFlow;
-               cryptoFlow=ICFProduct(_cryptoFlow);
-               cryptoFlow.invest{value: msg.value}(momsWallet,msg.value);
-       }
+    constructor(address payable _momsWallet, address _cryptoFlow) public payable {
+       momsWallet = _momsWallet;
+       cryptoFlowAddress = _cryptoFlow;
+       cryptoFlow=ICFProduct(_cryptoFlow);
+       cryptoFlow.invest{value: msg.value}(momsWallet,msg.value);
+    }
     
-    
-        
-        function earningstodate() public view returns(uint256){
-            return cryptoFlow.earningstodate(momsWallet);
+    function earningstodate() public view returns(uint256){
+        return cryptoFlow.getEarningsToDate(momsWallet);
             
-        }
-        function cashoutinvestment() public returns(uint256){
-            return cryptoFlow.cashoutinvestment(momsWallet);            
-        }
+    }
+    
+    function cashoutinvestment() public returns(uint256){
+        return cryptoFlow.withdraw(momsWallet);            
+    }
     
 }
+
